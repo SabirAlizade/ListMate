c97e3da601b036f4bdfa357d9687f570952be87c
Sabir <salizade@outlook.com>
2023-12-17 17:33:02 +0400

    Initial Commit

---
+++ ListMate/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ListMate
+//
+//  Created by Sabir Alizade on 17.12.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
